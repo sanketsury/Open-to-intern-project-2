def5549423e44a817d0a9abd7f025ba54ba250dc
Vikas Yadav <vikasyadav14nov@gmail.com>
2022-09-15 21:20:11 +0530

    final done

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -3,6 +3,11 @@ const isValidName = new RegExp(/^[a-zA-Z ]{2,50}$/)
 const isValidMobile = new RegExp(/^\(?([6-9]{1})\)?([0-9]{2})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/)
 const isValidEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
+const isValidCollegeName = function (value) {
+    let nameRegex =
+    /^[a-z ,.'-]+$/i;
+    if (nameRegex.test(value)) return true;
+  };
 
 const isValid = function (value) {
     if (typeof value === "undefined" || value === null) return false;
@@ -16,4 +21,4 @@ function isValidUrl(value) {
     return regexp.test(value);
 }
 
-module.exports = { isValid, isValidUrl, isValidName, isValidMobile, isValidEmail };
+module.exports = { isValid, isValidUrl, isValidName, isValidCollegeName, isValidMobile, isValidEmail };
