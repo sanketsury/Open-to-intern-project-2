f22b721a00db44d9b18525667d9c8ccc7ff71dcf
rahulravidas024 <rahulravidas024@gmail.com>
2022-09-15 13:11:47 +0530

    add some changes

--- src/controllers/collegeController.js
+++ src/controllers/collegeController.js
@@ -2,8 +2,6 @@ const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel");
 const { isValid, isValidUrl, isValidName } = require("../validations/validation");
 
-
-
 const createColleges = async function (req, res) {
     try {
         const { name, fullName, logoLink, isDeleted } = req.body;
@@ -11,27 +9,27 @@ const createColleges = async function (req, res) {
             return res.status(400).send({ status: false, message: "Data should be provided" })
         }
         if (isValid(name) == false) {
-            return res.status(400).send({ status: false, msg: "Short name is required" });
+            return res.status(400).send({ status: false, message: "Abbreviation is required" });
         }
         if (isValidName.test(name) == false) {
             return res.status(400).send({ status: false, message: "Only alphabets are allowed in name" })
         }
         if (isValid(fullName) == false) {
-            return res.status(400).send({ status: false, msg: "College name is required" });
+            return res.status(400).send({ status: false, message: "College name is required" });
         }
         if (isValidName.test(fullName) == false) {
             return res.status(400).send({ status: false, message: "Only alphabets are allowed in college name" })
         }
         if (isValid(logoLink) == false) {
-            return res.status(400).send({ status: false, msg: "Logo link is required" });
+            return res.status(400).send({ status: false, message: "Logo link is required" });
         }
         if (isValidUrl.test(logoLink) == false) {
-            return res.status(400).send({ status: false, msg: "URL is wrong" });
+            return res.status(400).send({ status: false, message: "URL is wrong" });
         }
 
         const duplicate = await collegeModel.findOne({ name: name });
         if (duplicate) {
-            return res.status(400).send({ status: false, msg: "The college is already present" });
+            return res.status(400).send({ status: false, message: "The college is already present" });
         }
 
         const colleges = await collegeModel.create(req.body);
@@ -46,23 +44,27 @@ const getCollegeDetails = async function (req, res) {
     try {
         const obj = {}
         const collegeName = req.query.collegeName
-        if (!collegeName) return res.status(400).send({ status: false, msg: "Please enter college Name" })
+        if (!collegeName) return res.status(400).send({ status: false, message: "Please enter college Name in Abbreviation" })
 
         const collegeID = await collegeModel.findOne({ name: collegeName }).select({ _id: 1 })
-        if (!collegeID) return res.status(404).send({ status: false, msg: "College Not Found" })
+        if (!collegeID) return res.status(404).send({ status: false, message: "College Not Found" })
 
-        const internData = await internModel.find({ collegeId: collegeID }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
+        let internData = await internModel.find({ collegeId: collegeID }).select({ _id: 1, name: 1, email: 1, mobile: 1 })
+        if(internData.length==0){
+            internData = "There is no such intern in this college"
+        }
+        const countInternData = await internModel.find({ collegeId: collegeID }).count()
         const getData = await collegeModel.findOne({ name: collegeName })
 
         obj.name = getData.name
         obj.fullName = getData.fullName
         obj.logoLink = getData.logoLink
+        obj.totalInterns = countInternData
         obj.interns = internData
-        if ((obj.interns).length == 0) return res.status(404).send({ status: false, data: obj, msg: "There is no such intern in this college" })
         res.status(200).send({ status: true, data: obj })
     }
     catch (err) {
-        return res.status(500).send({ status: false, data: err.message });
+        return res.status(500).send({ status: false, message: err.message });
     }
 }
 
--- src/validations/validation.js
+++ src/validations/validation.js
@@ -3,12 +3,16 @@ const isValidName = new RegExp(/^[a-zA-Z ]{2,50}$/)
 const isValidMobile = new RegExp("^(\\d{3}[- ]?){2}\\d{4}$")
 const isValidEmail = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 
+const isValidUrl = new RegExp("((http|https)://)(www.)?"
++ "[a-zA-Z0-9@:%._\\+~#?&//=]"
++ "{2,256}\\.[a-z]"
++ "{2,6}\\b([-a-zA-Z0-9@:%"
++ "._\\+~#?&//=]*)")
+
 const isValid = function (value) {
     if (typeof value === "undefined" || value === null) return false;
     if (typeof value === "string" && value.trim().length === 0) return false;
     return true;
 };
-
-const isValidUrl = new RegExp(/^((https?):\/\/)?([w|W]{3}\.)+[a-zA-Z0-9\-\.]{3,}\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/);
 
 module.exports = { isValid, isValidUrl, isValidName, isValidMobile, isValidEmail };
